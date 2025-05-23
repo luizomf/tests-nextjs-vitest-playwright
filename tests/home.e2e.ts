18b5a6415dd6936b942caaba1f28994f6e62eb18
Luiz Otávio <luizomf@gmail.com>
2025-05-23 09:29:37 -0300

    getByRole, beforeEach e afterAll no Playwright com Next.js

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -1,12 +1,50 @@
-import { test, expect } from '@playwright/test';
+import { makeTestTodoRepository } from '@/core/__tests__/utils/make-test-todo-repository';
+import { test, expect, Page } from '@playwright/test';
+
+const HOME_URL = '/';
+const HEADING = 'Lista de tarefas';
+const INPUT = 'Tarefa';
+const BUTTON = 'Criar tarefa';
+const BUTTON_BUSY = 'Criando tarefa...';
+const NEW_TODO_TEXT = 'New Todo';
+
+const getHeading = (p: Page) => p.getByRole('heading', { name: HEADING });
+const getInput = (p: Page) => p.getByRole('textbox', { name: INPUT });
+const getBtn = (p: Page) => p.getByRole('button', { name: BUTTON });
+const getBtnBusy = (p: Page) => p.getByRole('button', { name: BUTTON_BUSY });
+
+const getAll = (p: Page) => ({
+  heading: getHeading(p),
+  input: getInput(p),
+  btn: getBtn(p),
+  btnBusy: getBtnBusy(p),
+});
+
+test.beforeEach(async ({ page }) => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+
+  await page.goto(HOME_URL);
+});
+
+test.afterAll(async () => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+});
 
 test.describe('<Home /> (E2E)', () => {
   // Renderização
   test.describe('Renderização', () => {
     test('deve ter o title html correto', async ({ page }) => {
-      await page.goto('/');
+      await expect(page).toHaveTitle('Testes com Vitest e Playwright');
+    });
 
-      await page.waitForTimeout(5000);
+    test('deve renderizar o cabeçalho, o input e o botão para criar TODOs', async ({
+      page,
+    }) => {
+      await expect(getHeading(page)).toBeVisible();
+      await expect(getInput(page)).toBeVisible();
+      await expect(getBtn(page)).toBeVisible();
     });
   });
 
