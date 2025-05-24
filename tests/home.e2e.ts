4e50f795515fffad43b326330c98d8fa0a6fd366
Luiz Otávio <luizomf@gmail.com>
2025-05-24 15:19:47 -0300

    Teste E2E de remoção de elementos da página com o Playwright

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -139,17 +139,54 @@ test.describe('<Home /> (E2E)', () => {
     });
 
     test('deve limpar o input após criar um todo', async ({ page }) => {
-      //
+      const { btn, input } = getAll(page);
+      await input.fill(NEW_TODO_TEXT);
+      await btn.click();
+
+      await expect(input).toHaveValue('');
     });
   });
 
   test.describe('Exclusão', () => {
     test('deve permitir apagar um todo', async ({ page }) => {
-      //
+      const todos = await insertTestTodos();
+      await page.reload(); // make next.js revalidate cache
+
+      const itemToDelete = page
+        .getByRole('listitem')
+        .filter({ hasText: todos[1].description });
+      await expect(itemToDelete).toBeVisible();
+
+      const deleteBtn = itemToDelete.getByRole('button');
+      await deleteBtn.click();
+
+      await itemToDelete.waitFor({ state: 'detached' });
+      await expect(itemToDelete).not.toBeVisible();
     });
 
     test('deve permitir apagar todos os TODOs', async ({ page }) => {
-      //
+      await insertTestTodos();
+      await page.reload(); // make next.js revalidate cache
+
+      while (true) {
+        const item = page.getByRole('listitem').first();
+        const isVisible = await item.isVisible().catch(() => false);
+        if (!isVisible) break;
+
+        const text = await item.textContent();
+        if (!text) {
+          throw Error('Item text not found');
+        }
+
+        const deleteBtn = item.getByRole('button');
+        await deleteBtn.click();
+
+        const renewedItem = page
+          .getByRole('listitem')
+          .filter({ hasText: text });
+        await renewedItem.waitFor({ state: 'detached' });
+        await expect(renewedItem).not.toBeVisible();
+      }
     });
 
     test('deve desativar os items da lista enquanto envia a action', async ({
